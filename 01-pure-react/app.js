8837712185f09d107cc339aa6507542b16caeb34
ameya-6964 <belvalkar7@gmail.com>
2023-09-14 21:45:03 +0530

    Completed Section 1 Part 3 Of The Course

--- 01-pure-react/app.js
+++ 01-pure-react/app.js
@@ -1,4 +1,9 @@
 //Creating h1 tag Inside React
-const heading = React.createElement("h1", {}, "Hello World From React⚛️");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React⚛️"
+);
+console.log(heading); //!This Will Return Object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(heading); //! This Method Is Basically converting Object into HTML Element
