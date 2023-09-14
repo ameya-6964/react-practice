03fd2b63407e9498c259b6e4c059f45b52baa4be
ameya-6964 <belvalkar7@gmail.com>
2023-09-14 21:32:22 +0530

    Seperated App.js and Index1.html

---
+++ 01-pure-react/app.js
@@ -0,0 +1,4 @@
+//Creating h1 tag Inside React
+const heading = React.createElement("h1", {}, "Hello World From React⚛️");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
