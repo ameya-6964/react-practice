d30afde6602b48518cab6794dac967e45c428b31
ameya-6964 <belvalkar7@gmail.com>
2023-09-17 22:38:04 +0530

    Changes

--- 01-pure-react/app.js
+++ 01-pure-react/app.js
@@ -1,9 +1,32 @@
 //Creating h1 tag Inside React
-const heading = React.createElement(
+/* const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello World From React⚛️"
 );
-console.log(heading); //!This Will Return Object
+console.log(heading); //!This Will Return Object Which React Element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); //! This Method Is Basically converting Object into HTML Element
+root.render(heading); //! This Method Is Basically converting Object into HTML Element Which Browser Understand
+ */
+/*
+Create This Type Of Structure In React (Nested Elements)
+ <div id="parent">
+          <div id="child">
+                    <h1>
+                         Hello World
+                   </h1>
+          </div>
+ </div>
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "children" },
+    React.createElement("h1", {}, "Hello World")
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); //! This Method Is Basically converting Object into HTML Element Which Browser Understand
