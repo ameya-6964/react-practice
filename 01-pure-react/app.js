d7c5b61603c938bf307a423fa7d81330fbe97494
ameya-6964 <belvalkar7@gmail.com>
2023-09-18 17:11:44 +0530

    Understood React Is Library Not a Framework

--- 01-pure-react/app.js
+++ 01-pure-react/app.js
@@ -25,7 +25,7 @@ const parent = React.createElement(
   React.createElement(
     "div",
     { id: "children" },
-    React.createElement("h1", {}, "Hello World")
+    React.createElement("h1", {}, "Hello World From React.createElement")
   )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
