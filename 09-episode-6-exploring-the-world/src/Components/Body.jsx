83ba1001e3f7840d082df312e7e93aeabf140e31
ameya-6964 <belvalkar7@gmail.com>
2023-09-22 15:26:30 +0530

    Completed Assignment 5

--- 09-episode-6-exploring-the-world/src/Components/Body.jsx
+++ 09-episode-6-exploring-the-world/src/Components/Body.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resObj } from "../utils/MOCK-DATA";
+import SearchIcon from "../search.svg";
 
 const Body = () => {
   //! State variable - Super Powerful Variable => For That We Use React Hooks
   const [list, setList] = useState(resObj);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //! Search Restaurant By Name Function
+  const searchRestaurant = () => {
+    const filteredList = resObj.filter((res) =>
+      res.data.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setList(filteredList);
+  };
+
+  //! Clear Search Term Function
+  const clearSearch = () => {
+    setSearchTerm("");
+  };
 
   return (
     <div className="body">
-      <div className="filter">
-        <button
-          className="filter-button"
+      <div className="search">
+        <input
+          placeholder="Search For Restaurant"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="SearchBox"
           onClick={() => {
-            setList(resObj);
+            searchRestaurant();
+            clearSearch();
           }}
-        >
-          All Restaurants
-        </button>
+        />
+      </div>
+      <div className="filter">
         <button
           className="filter-button"
           onClick={() => {
@@ -26,6 +48,14 @@ const Body = () => {
           }}
         >
           Top Rated Restaurants 🔝
+        </button>
+        <button
+          className="filter-button"
+          onClick={() => {
+            setList(resObj);
+          }}
+        >
+          All Restaurants
         </button>
       </div>
       <div className="res-container">
