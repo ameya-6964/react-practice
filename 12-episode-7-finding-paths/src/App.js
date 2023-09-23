e7e363cd1a1c1cfbe7c6ca969aa42a77df3fccc2
ameya-6964 <belvalkar7@gmail.com>
2023-09-23 22:09:05 +0530

    Created Skelton Of Restaurant Menu and added Route

--- 12-episode-7-finding-paths/src/App.js
+++ 12-episode-7-finding-paths/src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header";
 import Body from "./Components/Body";
 import About from "./Components/About";
 import ContactUs from "./Components/ContactUs";
+import RestaurantMenu from "./Components/RestaurantMenu";
 import "../App.css";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Error from "./Components/Error";
@@ -32,6 +33,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <ContactUs />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
       },
     ],
     errorElement: <Error />,
---
+++ 12-episode-7-finding-paths/src/Components/RestaurantMenu.jsx
@@ -0,0 +1,15 @@
+const RestaurantMenu = () => {
+  return (
+    <div className="menu">
+      <h1>Name Of The Restaurant</h1>
+      <h2>Menu</h2>
+      <ul>
+        <li>Biryani</li>
+        <li>Burgers</li>
+        <li>Coke</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
