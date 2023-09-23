ed5a2fbe4239f793f8a245740b31b28cc8f2e62d
ameya-6964 <belvalkar7@gmail.com>
2023-09-24 03:05:20 +0530

    Added Price ANd Menu To The Screen

--- 12-episode-7-finding-paths/src/Components/RestaurantMenu.jsx
+++ 12-episode-7-finding-paths/src/Components/RestaurantMenu.jsx
@@ -1,14 +1,54 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.151546&lng=72.999448&restaurantId=84585&catalog_qa=undefined&submitAction=ENTER"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  console.log(itemCards);
+
   return (
-    <div className="menu">
-      <h1>Name Of The Restaurant</h1>
-      <h2>Menu</h2>
-      <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Coke</li>
-      </ul>
-    </div>
+    <>
+      <div className="menu">
+        <h1>Restaurant Name - {name}</h1>
+        <h2>
+          Cuisines
+          <p>
+            {cuisines.join(", ")} - Price : {costForTwoMessage}
+          </p>
+        </h2>
+      </div>
+      <div className="menu-items">
+        <h1>Menu</h1>
+        <ul>
+          {itemCards.map((item) => (
+            <li key={item.card.info.id}>
+              {item.card.info.name} - ₹&nbsp;
+              {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
