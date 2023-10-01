130cbb5e518e34a5bed9216be689ce576a3ec1a1
ameya-6964 <belvalkar7@gmail.com>
2023-10-01 16:50:27 +0530

    Completed episode 8 Part 2

--- 13-episode-lets-get-classy/src/Components/User.jsx
+++ 13-episode-lets-get-classy/src/Components/User.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const User = ({ name, location, contact }) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(10);
+  //! This is Function For Count1 variable To Increase Value By 1
+  function increment() {
+    setCount(count + 1);
+  }
+  //! This is Function For Count1 variable To Decrease Value By 1
+  function decrement() {
+    setCount(count - 1);
+  }
+  //! This is Function For Count2 variable To Increase Value By 5
+  //! If You Have Initialised value rather then 0 use this way
+  function increment2() {
+    setCount2((prevCount) => prevCount + 5);
+  }
+  //! This is Function For Count2 variable To Decrease Value By 5
+  //! If You Have Initialised value rather then 0 use this way
+  function decrement2() {
+    setCount2((prevCount) => prevCount - 5);
+  }
   return (
     <div className="user-container">
-      <h2> Name: {name}</h2>
-      <h3> Location: {location}</h3>
-      <h4> Contact: {contact}</h4>
+      <h1> Name: {name}</h1>
+      <h2> Location: {location}</h2>
+      <h3> Contact: {contact}</h3>
+      <h3> Count1 : {count}</h3>
+      <h3> Count2 : {count2}</h3>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <button onClick={increment2}>+</button>
+      <button onClick={decrement2}>-</button>
     </div>
   );
 };
--- 13-episode-lets-get-classy/src/Components/UserClass.jsx
+++ 13-episode-lets-get-classy/src/Components/UserClass.jsx
@@ -8,16 +8,26 @@ class UserClass extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 10,
+    };
   }
 
   //!  Only Diffrence Between Class Based And Fumctional Component
   render() {
     const { name, location, contact } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-container">
-        <h2> Name: {name}</h2>
-        <h3> Location: {location}</h3>
-        <h4> Contact: {contact}</h4>
+        <h1> Name: {name}</h1>
+        <h2> Location: {location}</h2>
+        <h3> Contact: {contact}</h3>
+        <h3> Count1 : {count}</h3>
+        <h3> Count2 : {count2}</h3>
+        <button>+</button>
+        <button>-</button>
       </div>
     );
   }
