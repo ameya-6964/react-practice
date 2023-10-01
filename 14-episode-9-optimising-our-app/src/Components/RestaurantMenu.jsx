c37e040241f4f5bc018197fe2fc5540db0932495
ameya-6964 <belvalkar7@gmail.com>
2023-10-01 23:29:35 +0530

    Created My First Custum Hook and Implemented It

--- 14-episode-9-optimising-our-app/src/Components/RestaurantMenu.jsx
+++ 14-episode-9-optimising-our-app/src/Components/RestaurantMenu.jsx
@@ -1,27 +1,15 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
   const { resId } = useParams();
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage } =
     resInfo?.cards[0]?.card?.card?.info;
-
   const { itemCards } =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
