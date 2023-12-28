b5a2f676b24bb2f1a7bec4bea7a7e4300bbcd514
ameya-6964 <belvalkar7@gmail.com>
2023-12-29 02:27:32 +0530

    fixed Classname

--- Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/Stats.jsx
+++ Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/Stats.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Stats = () => {
   return (
-    <footer>
+    <footer className="stats">
       <em>
         You Have X Items On Your List , and You Have Already Packed X (x%)
       </em>
