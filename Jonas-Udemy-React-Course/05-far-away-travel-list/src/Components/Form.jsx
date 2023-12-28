69a09711bebb20193cf6326912826ec30c12ac60
ameya-6964 <belvalkar7@gmail.com>
2023-12-29 03:07:12 +0530

    Completed Section 6

--- Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/Form.jsx
+++ Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/Form.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target[1].value);
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  };
+
   return (
-    <div className="add-form">
-      <h3>What Do You Need For Your Trip 🤔</h3>
-    </div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What Do You Need For Your Trip ? 🤔</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 };
 
--- Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/PackingList.jsx
+++ Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/PackingList.jsx
@@ -4,7 +4,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Chargers", quantity: 1, packed: true },
-  { id: 3, description: "Medicines", quantity: 1, packed: false },
+  { id: 4, description: "Medicines", quantity: 1, packed: false },
 ];
 const PackingList = () => {
   return (
