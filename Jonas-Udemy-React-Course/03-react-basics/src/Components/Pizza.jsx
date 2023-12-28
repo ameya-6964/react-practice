845a33af23f564faab57ca421cdeb5e646ebe3b2
ameya-6964 <belvalkar7@gmail.com>
2023-12-28 16:50:25 +0530

    Setting Classes And Text Conditionally

--- Jonas-Udemy-React-Course/03-react-basics/src/Components/Pizza.jsx
+++ Jonas-Udemy-React-Course/03-react-basics/src/Components/Pizza.jsx
@@ -2,14 +2,12 @@ const PizzaData = ({ pizza }) => {
   const { name: pizzaName, photoName, ingredients, price, soldOut } = pizza;
   console.log(soldOut);
   return (
-    <li className="pizza">
+    <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
       <img src={`../../${photoName}`} alt={pizzaName} />
       <div>
         <h3>{pizzaName}</h3>
         <p>{ingredients}</p>
-        <span>
-          <b> {price}</b> ₹
-        </span>
+        {soldOut ? <span>SOLD OUT</span> : <span>{price}</span>}
       </div>
     </li>
   );
