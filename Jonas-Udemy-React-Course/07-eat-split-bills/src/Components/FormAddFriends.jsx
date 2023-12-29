901cd87c0284d298a94e785cba48be76117c03dc
ameya-6964 <belvalkar7@gmail.com>
2023-12-30 01:01:44 +0530

    Added Function and State To Create New Friend

--- Jonas-Udemy-React-Course/07-eat-split-bills/src/Components/FormAddFriends.jsx
+++ Jonas-Udemy-React-Course/07-eat-split-bills/src/Components/FormAddFriends.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const FormAddFriends = () => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = crypto.randomUUID();
+    if (!name || !image) return;
+
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  };
+
   return (
-    <form className="form-add-friend">
-      <label htmlFor="name">Friend Name</label>
-      <input type="text" id="name" />
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>Friend Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label>Image Url</label>
-      <input type="text" id="name" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
