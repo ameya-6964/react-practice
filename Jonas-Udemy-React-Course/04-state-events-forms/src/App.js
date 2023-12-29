8612addf6c3e2b419bf1689698c86ede22191e10
ameya-6964 <belvalkar7@gmail.com>
2023-12-29 22:00:16 +0530

    Created Children Prop fro StepMessage

--- Jonas-Udemy-React-Course/04-state-events-forms/src/App.js
+++ Jonas-Udemy-React-Course/04-state-events-forms/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./Button";
+import StepMessage from "./StepMessage";
 
 const messages = [
   "Learn React ⚛️",
@@ -37,9 +38,7 @@ function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]} </StepMessage>
           <div className="buttons">
             <Button
               bgColor="#7950f2"
