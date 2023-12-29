cac645f61e060307b890ebdcf72f4541695c067a
ameya-6964 <belvalkar7@gmail.com>
2023-12-30 00:45:04 +0530

    Created showAddFriend Handler Function To Display Add Freind Form

--- Jonas-Udemy-React-Course/07-eat-split-bills/src/App.js
+++ Jonas-Udemy-React-Course/07-eat-split-bills/src/App.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import Button from "./Components/Button";
 import FormAddFriends from "./Components/FormAddFriends";
 import FormSplitBill from "./Components/FormSplitBill";
 import FriendsList from "./Components/FriendsList";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  const handleShowAddFriend = () => {
+    setShowAddFriend((show) => !show);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriends />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriends />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
