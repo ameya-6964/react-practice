68262c0876d4f7adfe082e39d233297673358341
ameya-6964 <belvalkar7@gmail.com>
2023-12-29 15:45:17 +0530

    Added OnToggle Item => parent to child => child To Child

--- Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/Item.jsx
+++ Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/Item.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Item = ({ item, onDeleteItem }) => {
+const Item = ({ item, onDeleteItem, onToggleItems }) => {
   const { quantity, description, packed } = item;
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItems(item.id);
+        }}
+      />
       <span style={packed ? { textDecoration: "line-through" } : {}}>
         {quantity} {description}
       </span>
--- Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/PackingList.jsx
+++ Jonas-Udemy-React-Course/05-far-away-travel-list/src/Components/PackingList.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Item from "./Item";
 
-const PackingList = ({ items, onDeleteItem }) => {
+const PackingList = ({ items, onDeleteItem, onToggleItems }) => {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItems={onToggleItems}
+          />
         ))}
       </ul>
     </div>
