2b53098f7e87a023f0caa0be83d12bffccf9f6ed
ameya-6964 <belvalkar7@gmail.com>
2023-12-31 18:06:18 +0530

    Implemented Error Handling

--- Jonas-Udemy-React-Course/08-usepopcorn-project/src/App.jsx
+++ Jonas-Udemy-React-Course/08-usepopcorn-project/src/App.jsx
@@ -7,24 +7,38 @@ import Box from "./Components/Box";
 import MovieList from "./Components/MovieList";
 import WatchSummary from "./Components/WatchSummary";
 import WatchMoviesList from "./Components/WatchMovieList";
+import Loading from "./Components/Loading";
+import ErrorMessage from "./Components/ErrorMessage";
 
 const apiKey = import.meta.env.VITE_MOVIE_KEY;
-const searchString = `avengers`;
+const searchString = `Titanic`;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     fetchMovie();
   }, []);
 
   const fetchMovie = async () => {
-    const res = await fetch(
-      `https://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${searchString}`
-    );
-    const data = await res.json();
-    setMovies(data.Search);
+    try {
+      setIsLoading(true);
+      const res = await fetch(
+        `https://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${searchString}`
+      );
+      if (!res.ok) throw new Error("Something Went Wrong With Fetching Movies");
+      const data = await res.json();
+      if (data.Response === "False") throw new Error("Movie Not Found");
+      setMovies(data.Search);
+      console.log(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -36,7 +50,10 @@ const App = () => {
 
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {/* {isLoading ? <Loading /> : <MovieList movies={movies} />} */}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
+          {isLoading && <Loading />}
         </Box>
 
         <Box>
---
+++ Jonas-Udemy-React-Course/08-usepopcorn-project/src/Components/Loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
