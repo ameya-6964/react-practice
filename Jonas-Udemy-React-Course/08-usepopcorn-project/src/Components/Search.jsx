32b7ffa821bbfe8db8c54d55f5df2a6ad7b8d6c6
ameya-6964 <belvalkar7@gmail.com>
2024-01-02 00:58:31 +0530

    Added useRef Hook To Focus Input Field On Pressing Enter

--- Jonas-Udemy-React-Course/08-usepopcorn-project/src/Components/Search.jsx
+++ Jonas-Udemy-React-Course/08-usepopcorn-project/src/Components/Search.jsx
@@ -1,4 +1,33 @@
+import { useEffect, useRef } from "react";
+
 const Search = ({ query, setQuery }) => {
+  //! How Not To Select DOM Elements
+  /*   useEffect(function () {
+    const el = document.querySelector(".search");
+    el.focus();
+  }, []); */
+
+  //* How To Correctly Select DOM Elements
+  const inputEl = useRef(null);
+  useEffect(
+    function () {
+      const enterButtonPressed = (e) => {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      };
+
+      document.addEventListener("keydown", enterButtonPressed);
+
+      return () => {
+        document.removeEventListener("keydown", enterButtonPressed);
+      };
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -6,6 +35,7 @@ const Search = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
