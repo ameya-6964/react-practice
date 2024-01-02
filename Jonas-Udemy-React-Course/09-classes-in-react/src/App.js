528cadc2fc750e07b45ed8e57212a942efadacd1
ameya-6964 <belvalkar7@gmail.com>
2024-01-02 16:42:45 +0530

    Created State With Class Based Components

--- Jonas-Udemy-React-Course/09-classes-in-react/src/App.js
+++ Jonas-Udemy-React-Course/09-classes-in-react/src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 5,
+    };
+  }
+
+  render() {
+    return (
+      <div className="app-demo">
+        <button>+</button>
+        <h1>{this.state.count}</h1>
+        <button>-</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
