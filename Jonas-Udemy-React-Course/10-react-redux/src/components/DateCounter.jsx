c7776a8a706622489893e11dec3c8d9fca67e728
ameya-6964 <belvalkar7@gmail.com>
2024-01-03 00:44:36 +0530

    Created 1 useReducer Hook

--- Jonas-Udemy-React-Course/10-react-redux/src/components/DateCounter.jsx
+++ Jonas-Udemy-React-Course/10-react-redux/src/components/DateCounter.jsx
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  if (action.type === "inc") {
+    return state + 1;
+  }
+  if (action.type === "dec") {
+    return state - 1;
+  }
+  if (action.type === "setCount") {
+    return action.payload;
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
+
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -10,25 +24,29 @@ function DateCounter() {
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    //setCount((count) => count - step);
+    if (count === 0) return;
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    //setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    //setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    //setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    //setCount(0);
+    //setStep(1);
   };
 
   return (
