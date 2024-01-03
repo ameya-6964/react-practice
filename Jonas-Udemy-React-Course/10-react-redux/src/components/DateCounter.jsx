499013dbd0d0a7a739b405f354b99a3814ec2b10
ameya-6964 <belvalkar7@gmail.com>
2024-01-03 18:06:55 +0530

    Created Second useReducer Function

--- Jonas-Udemy-React-Course/10-react-redux/src/components/DateCounter.jsx
+++ Jonas-Udemy-React-Course/10-react-redux/src/components/DateCounter.jsx
@@ -1,53 +1,47 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action) {
-  if (action.type === "inc") {
-    return state + 1;
-  }
-  if (action.type === "dec") {
-    return state - 1;
-  }
-  if (action.type === "setCount") {
-    return action.payload;
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+
+    default:
+      throw new Error("Unknown Action");
   }
 }
 
 function DateCounter() {
-  //const [count, setCount] = useState(0);
+  const initialState = { count: 0, step: 0 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
-
-  // This mutates the date object.
   const date = new Date("january 03 2024");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    //setCount((count) => count - step);
     if (count === 0) return;
     dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    //setCount((count) => count + step);
     dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    //setCount(Number(e.target.value));
     dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    //setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
-  const reset = function () {
-    //setCount(0);
-    //setStep(1);
-  };
+  const reset = function () {};
 
   return (
     <div className="counter">
