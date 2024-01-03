dd1bc1ebff32137ecf24ef74f894b1eba9c52a73
ameya-6964 <belvalkar7@gmail.com>
2024-01-03 22:42:47 +0530

    Fetched Data Through Useeffect

--- Jonas-Udemy-React-Course/10-react-usereducer/src/App.jsx
+++ Jonas-Udemy-React-Course/10-react-usereducer/src/App.jsx
@@ -1,7 +1,42 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action Is Unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function getQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    getQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
