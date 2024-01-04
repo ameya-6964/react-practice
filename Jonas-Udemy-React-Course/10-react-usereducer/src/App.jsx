a37ee7e1a9aaedd4063d34606d1bf59459795bfb
ameya-6964 <belvalkar7@gmail.com>
2024-01-04 14:50:29 +0530

    Done With Finish Screen

--- Jonas-Udemy-React-Course/10-react-usereducer/src/App.jsx
+++ Jonas-Udemy-React-Course/10-react-usereducer/src/App.jsx
@@ -9,6 +9,7 @@ import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
 import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
 
 const initialState = {
   questions: [],
@@ -16,6 +17,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -43,16 +45,22 @@ function reducer(state, action) {
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
 
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.highscore : state.points,
+      };
+
     default:
       throw new Error("Action Is Unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -96,8 +104,21 @@ function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
         )}
       </Main>
     </div>
---
+++ Jonas-Udemy-React-Course/10-react-usereducer/src/components/FinishScreen.jsx
@@ -0,0 +1,23 @@
+const FinishScreen = ({ points, maxPossiblePoints, highscore }) => {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage >= 90) emoji = "🎖️";
+  if (percentage >= 80 && percentage < 90) emoji = "🥇";
+  if (percentage >= 60 && percentage < 80) emoji = "🥈";
+  if (percentage >= 50 && percentage < 60) emoji = "🥉";
+  if (percentage <= 50 && percentage > 35) emoji = "😃";
+  if (percentage <= 35) emoji = "🤦🤦‍♀️";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You Scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} Points)</p>
+    </>
+  );
+};
+
+export default FinishScreen;
