fe3f734e6640103bff1b8b58fec9389bfdeb4101
ameya-6964 <belvalkar7@gmail.com>
2024-01-07 22:06:25 +0530

    Implemeted CSS Modules

--- Jonas-Udemy-React-Course/11-single-page-application/src/pages/Homepage.jsx
+++ Jonas-Udemy-React-Course/11-single-page-application/src/pages/Homepage.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+
 const Homepage = () => {
   return (
     <div>
       <PageNav />
+
       <h1>Home Page</h1>
+      <Link to="/app">Go To Application</Link>
     </div>
   );
 };
