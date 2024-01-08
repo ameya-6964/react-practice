bdecfd4afdd90133c32078150fdaf2a3a3ecb623
ameya-6964 <belvalkar7@gmail.com>
2024-01-08 23:52:33 +0530

    Created Nested Routes

--- Jonas-Udemy-React-Course/11-single-page-application/src/components/Sidebar.jsx
+++ Jonas-Udemy-React-Course/11-single-page-application/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List Of Cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} By WorldWise INC.
