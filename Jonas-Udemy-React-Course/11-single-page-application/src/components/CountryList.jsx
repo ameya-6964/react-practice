38c34fb055f5b9b4a8c31ccac213328d91e81965
ameya-6964 <belvalkar7@gmail.com>
2024-01-09 01:36:37 +0530

    Completed CountryList and CountryItem

---
+++ Jonas-Udemy-React-Course/11-single-page-application/src/components/CountryList.jsx
@@ -0,0 +1,27 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+const CountryList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  if (!countries.length)
+    return <Message message={"Add Your First City By Clicking On Map"} />;
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+};
+
+export default CountryList;
