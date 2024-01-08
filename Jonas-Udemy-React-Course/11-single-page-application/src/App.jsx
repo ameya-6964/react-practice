61bc22bea29079557e6ad31f11234dcf5c8fd073
ameya-6964 <belvalkar7@gmail.com>
2024-01-09 02:17:15 +0530

    feature : AddedProgramatic Navigation Using useNavigate Hook

--- Jonas-Udemy-React-Course/11-single-page-application/src/App.jsx
+++ Jonas-Udemy-React-Course/11-single-page-application/src/App.jsx
@@ -9,6 +9,7 @@ import City from "./components/City";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -53,7 +54,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
