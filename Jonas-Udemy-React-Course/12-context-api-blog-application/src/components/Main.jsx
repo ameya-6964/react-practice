a16075cce89af109c1dee983837055fe1606bdde
ameya-6964 <belvalkar7@gmail.com>
2024-01-09 16:35:12 +0530

    Removed Prop Drilling Drom App Component and Replaced With ContextAPI

--- Jonas-Udemy-React-Course/12-context-api-blog-application/src/components/Main.jsx
+++ Jonas-Udemy-React-Course/12-context-api-blog-application/src/components/Main.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import FormAddPost from "./FormAddPost";
 import Posts from "./Posts";
+import { PostContext } from "../App";
 
-function Main({ posts, onAddPost }) {
+function Main() {
+  const { posts, onAddPost } = useContext(PostContext);
   return (
     <main>
       <FormAddPost onAddPost={onAddPost} />
