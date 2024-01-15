a9823805e5bd3cd2a00ff8313bf1cbabd5fb2d3c
ameya-6964 <belvalkar7@gmail.com>
2024-01-15 20:07:24 +0530

    Rendered Children Route in AppLayout

---
+++ Jonas-Udemy-React-Course/14-pizza-app/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Link to="/"> Fast React Pizza Company</Link>
+      <p>Ameya</p>
+    </div>
+  );
+};
+
+export default Header;
